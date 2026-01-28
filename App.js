106e0d06208c6cfd87f9ec941e222c6a027bd0a9
M3code <abdullamatter3@gmail.com>
2026-01-28 15:16:38 +0300

    Task18

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Text, View, ScrollView, TextInput } from 'react-native';
 import Task16 from './Tasks/Task16';
 import Task17 from './Tasks/Task17';
+import Task18 from './Tasks/Task18';
 
 function App() {
   return (
-    <View>
-      {
-        //<Task16></Task16>
-      }
-      <Task17></Task17>
+    <View style={{ flex: 1 }}>
+      {/*
+        <Task16></Task16>
+        <Task17></Task17>
+      */}
+      <Task18></Task18>
     </View>
   );
 }
---
+++ Tasks/Task18.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, ActivityIndicator, StyleSheet } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function Task18() {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => setIsVisible(true), 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {!isVisible ? (
+        <>
+          <ActivityIndicator size="large" />
+          <Text>Loading ...</Text>
+        </>
+      ) : (
+        <Text>abdulla</Text>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
