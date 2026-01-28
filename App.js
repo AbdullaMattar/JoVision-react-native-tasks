580e4af9f94976d11781674b2e73c13a0dd3aa04
M3code <abdullamatter3@gmail.com>
2026-01-28 12:30:05 +0300

    Task 17 + fix 16

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Text, View, ScrollView, TextInput } from 'react-native';
 import Task16 from './Tasks/Task16';
+import Task17 from './Tasks/Task17';
 
-function YourApp() {
+function App() {
   return (
     <View>
-      <Task16></Task16>
+      {
+        //<Task16></Task16>
+      }
+      <Task17></Task17>
     </View>
   );
 }
 
-export default YourApp;
+export default App;
---
+++ Tasks/Task17.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Text, View, ScrollView, TextInput, Button } from 'react-native';
+
+export default function Task17() {
+  const [isVisible, setIsVisible] = useState(true);
+  return (
+    <View>
+      {isVisible && <Text>Abdulla Matar</Text>}
+      <Button title="Show" onPress={() => setIsVisible(!isVisible)} />
+      {/*another way- Text Component is still there*/}
+      <Text>{isVisible ? 'Abdulla Matar' : null}</Text>
+    </View>
+  );
+}
