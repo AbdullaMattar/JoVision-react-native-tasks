47aaa8ef188858d6a6e8ed24831533febd751ab9
M3code <abdullamatter3@gmail.com>
2026-01-29 14:58:19 +0300

    task23

---
+++ Tasks/Task23.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, Component } from 'react';
+import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function Task22() {
+  const [Tinput, setTinput] = useState('');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>{Tinput}</Text>
+      <MyClassPage setT={setTinput} />
+    </SafeAreaView>
+  );
+}
+
+class MyClassPage extends Component {
+  render() {
+    return (
+      <TextInput
+        placeholder="Type here"
+        onChangeText={newtext => this.props.setT(newtext)}
+      ></TextInput>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
