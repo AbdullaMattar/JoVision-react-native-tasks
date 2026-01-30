e68d74ef842d721f3de7eed7930313776410670e
M3code <abdullamatter3@gmail.com>
2026-01-30 14:08:52 +0300

    Task25

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import Task21 from './Tasks/Task21';
 import Task22 from './Tasks/Task22';
 import Task23 from './Tasks/Task23';
 import Task24 from './Tasks/Task24';
+import Task25 from './Tasks/Task25';
 function App() {
   return (
     <SafeAreaProvider>
@@ -24,8 +25,9 @@ function App() {
         <Task22 />
         <Task23 />
         <Task24 />
+        <Task25 />
       */}
-        <Task24 />
+        <Task25 />
       </View>
     </SafeAreaProvider>
   );
---
+++ Tasks/Task25.js
@@ -0,0 +1,48 @@
+import React, {
+  useState,
+  useEffect,
+  Component,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  createRef,
+} from 'react';
+import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function Task25() {
+  const myref = createRef(); // useRef(null) is also possible
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        placeholder="Type Here"
+        onChangeText={inputText => myref.current?.printText(inputText)} //myref.current? => Call printText only if current exists
+      />
+      <MyClassPage ref={myref} />
+    </SafeAreaView>
+  );
+}
+
+class MyClassPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { txt: '' };
+  }
+
+  printText = inputText => {
+    this.setState({ txt: inputText });
+  };
+
+  render() {
+    return <Text>{this.state.txt}</Text>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
